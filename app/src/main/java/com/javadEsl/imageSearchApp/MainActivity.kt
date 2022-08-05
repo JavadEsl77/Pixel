84bb23f6ee0a6e4c6369c769d5b73c046cbd886a
javadEsl <javades755@gmail.com>
2022-08-05 10:19:06 +0430

    [Fix] fix packageName

--- app/src/main/java/com/javadEsl/imageSearchApp/MainActivity.kt
+++ app/src/main/java/com/javadEsl/imageSearchApp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.javadEsl.imageSearchApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.JavadEsl77.imagesearchapp.R
 
 
 class MainActivity : AppCompatActivity() {
