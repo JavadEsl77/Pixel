fc4aef13f8e9f2c05b7eeeb5261e9c293dfd29e2
javadEsl <javades755@gmail.com>
2022-08-05 10:10:01 +0430

    Initial commit

---
+++ app/src/main/java/com/javadEsl/imageSearchApp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.javadEsl.imageSearchApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.JavadEsl77.imagesearchapp.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
