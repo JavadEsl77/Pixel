d40c701aaa39cb6a5a8d82de62842d53314d9132
javadEsl <javades755@gmail.com>
2022-08-06 12:18:54 +0430

    [FEATURE] Creating Navigation Component .

---
+++ app/src/main/java/com/javadEsl/imageSearchApp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.javadEsl.imageSearchApp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.javadEsl.imageSearchApp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
