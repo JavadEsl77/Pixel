8faf513d77b964eef0b1697582e7da4edbc9a1ff
javadEsl <javades755@gmail.com>
2022-08-15 17:47:30 +0430

    [FEATURE] add Search Functionality .

--- app/src/main/java/com/javadEsl/imageSearchApp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/javadEsl/imageSearchApp/ui/gallery/GalleryFragment.kt
@@ -1,14 +1,21 @@
 package com.javadEsl.imageSearchApp.ui.gallery
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView.OnEditorActionListener
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.javadEsl.imageSearchApp.R
-import com.javadEsl.imageSearchApp.data.UnsplashPhoto
 import com.javadEsl.imageSearchApp.databinding.FragmentGalleryBinding
-import com.javadEsl.imageSearchApp.databinding.UnsplashPhotoLoadStateFooterBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_gallery.*
+
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
@@ -27,9 +34,50 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
                 footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
             )
 
+            img_clean.setOnClickListener {
+                edtSearch.setText("")
+                imgClean.isVisible = false
+            }
+            edtSearch.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    imgClean.isVisible = edtSearch.text != null
+
+                }
+            })
+
+            edtSearch.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    val inputMethodManager =
+                        requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+                    performSearch()
+                    return@OnEditorActionListener true
+                }
+                false
+            })
+
         }
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    private fun performSearch() {
+        if (binding.edtSearch.text.toString().isEmpty()) {
+            binding.edtSearch.error = "please fill out this field"
+        } else {
+            viewModel.searchPhotos(binding.edtSearch.text.toString())
         }
     }
 
