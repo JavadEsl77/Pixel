46f3907b985af6ce8e5626c1997a971fb5c9bc1e
javadEsl <javades755@gmail.com>
2022-08-16 13:34:13 +0430

    [FEATURE] add Implementing the DetailsFragment.

--- app/src/main/java/com/javadEsl/imageSearchApp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/javadEsl/imageSearchApp/ui/details/DetailsFragment.kt
@@ -1,9 +1,74 @@
 package com.javadEsl.imageSearchApp.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.javadEsl.imageSearchApp.R
+import com.javadEsl.imageSearchApp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
+    private val args = navArgs<DetailsFragmentArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val photo = args.value.photo
+
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.full)
+                .error(R.drawable.ic_baseline_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        textViewCreator.isVisible = true
+                        textViewDescription.isVisible = photo.description != null
+                        return false
+                    }
+                })
+                .into(imageView)
+
+            textViewDescription.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            textViewCreator.apply {
+                text = "photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+    }
 
 }
