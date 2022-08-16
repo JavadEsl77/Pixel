13da956e7f153e968ea7cbcf229f4246c43ec587
javadEsl <javades755@gmail.com>
2022-08-16 18:21:53 +0430

    [FEATURE] add Handling Process Death

--- app/src/main/java/com/javadEsl/imageSearchApp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/javadEsl/imageSearchApp/ui/gallery/GalleryViewModel.kt
@@ -1,6 +1,6 @@
 package com.javadEsl.imageSearchApp.ui.gallery
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -10,10 +10,13 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class GalleryViewModel @Inject constructor(private val unsplashRepository: UnsplashRepository) :
+class GalleryViewModel @Inject constructor(
+    private val unsplashRepository: UnsplashRepository,
+    state: SavedStateHandle
+) :
     ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         unsplashRepository.getSearchResults(queryString).cachedIn(viewModelScope)
@@ -24,6 +27,7 @@ class GalleryViewModel @Inject constructor(private val unsplashRepository: Unspl
     }
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
 
