9408075cecccd23fa2a2594b9632ea17a3fb8c9d
javadEsl <javades755@gmail.com>
2022-09-11 18:17:42 +0430

    [FEATURE] add progress bar to gallery adapter

--- app/src/main/java/com/javadEsl/pixel/ui/gallery/UnsplashPhotoAdapter.kt
+++ app/src/main/java/com/javadEsl/pixel/ui/gallery/UnsplashPhotoAdapter.kt
@@ -1,14 +1,20 @@
 package com.javadEsl.pixel.ui.gallery
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.javadEsl.pixel.R
 import com.javadEsl.pixel.data.UnsplashPhoto
 import com.javadEsl.pixel.data.convertedUrl
@@ -46,6 +52,27 @@ class UnsplashPhotoAdapter(private val listener: OnItemClickListener) :
                     .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                     .transition(DrawableTransitionOptions.withCrossFade())
                     .error(R.drawable.ic_error_photos)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: com.bumptech.glide.load.DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBarImage.isVisible = false
+                            return false
+                        }
+                    })
                     .into(imageView)
 
 
