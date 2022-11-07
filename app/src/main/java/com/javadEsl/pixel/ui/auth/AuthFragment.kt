6c5a985a644aa1736585002d15d32f7b1c74d6b3
javadEsl <javades755@gmail.com>
2022-11-07 14:12:49 +0330

    [FEATURE] auth fragment and fragment_auth.xml

---
+++ app/src/main/java/com/javadEsl/pixel/ui/auth/AuthFragment.kt
@@ -0,0 +1,39 @@
+package com.javadEsl.pixel.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.javadEsl.pixel.R
+import com.javadEsl.pixel.databinding.FragmentAuthBinding
+import com.javadEsl.pixel.databinding.FragmentDetailsBinding
+
+class AuthFragment : Fragment(R.layout.fragment_auth) {
+
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<AuthViewModel>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        if (_binding == null) {
+            _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/javadEsl/pixel/ui/auth/AuthViewModel.kt
@@ -0,0 +1,8 @@
+package com.javadEsl.pixel.ui.auth
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class AuthViewModel() : ViewModel() {
+}
